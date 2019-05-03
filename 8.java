43502e18cce42c86be50a0df59a44d4510fb5862
VasilisDev <37515493+VasilisDev@users.noreply.github.com>
2019-05-03 21:04:29 +0300

    Add files via upload

---
+++ 8.java
@@ -0,0 +1,20 @@
+/*
+
+You need to find the max element in the collection. Just write your own implementation of the method
+public static Integer maxElem(List<Integer> list)
+
+***If you have any problems:
+View the documentation of class Collections
+search method
+Sample Input:
+
+847 271 663 473 376 133 693 13 382 879
+Sample Output:
+
+879
+
+*/
+
+public static Integer maxElem(List<Integer> list){
+        return Collections.max(list);
+    }
